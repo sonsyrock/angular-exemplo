945ebf715ebe6551e735f75e44680bb84c4b502c
sonsyrock <gabrisoares1020@hotmail.com>
2020-02-21 15:48:06 -0300

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/core';
+import {products} from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit { //No typescript, quando vc tá definindo um tipo (seja classe, enum, interface ou qualquer outro) e vc vai usar esse tipo em outro arquivo vc tem que exportar. Por isso existe esta classe está com a propriedade (export).
 
+  product;
+  
   constructor() { }
 
   ngOnInit() {
