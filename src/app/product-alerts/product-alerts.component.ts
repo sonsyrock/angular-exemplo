5c2da87cefe82a6826c1d5d501d94fd28a98c68c
sonsyrock <gabrisoares1020@hotmail.com>
2020-02-21 12:51:28 -0300

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {Input} from '@angular/core';//Esta linha importa o Ipunt da biblioteca, angular/core. Serve para receber um produto como entrada.
 
-@Component({
-  selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
+
+@Component({ //Esta linha indica que esta classe é um componente.
+  selector: 'app-product-alerts', //o selector é o nome que atribuimos a classe componente
+  templateUrl: './product-alerts.component.html', //Linha 7 e 8 são gerados pelo StackBlitz
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
 
+  @Input() product;//Não entendi bem esta linha. Acredito que está associando o Input a classe pai product
   constructor() { }
 
   ngOnInit() {
