78181589831e7170570817b66545e8be6d6e405c
sonsyrock <gabrisoares1020@hotmail.com>
2020-02-21 13:05:53 -0300

    2 files modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Input} from '@angular/core';//Esta linha importa o Ipunt da biblioteca, angular/core. Serve para receber um produto como entrada.
+import {Output, EventEmitter} from '@angular/core';//Nesta linha, importamos as bibliotecas Output e EventEmitter para que o botão (Notificar) prensente no arquivo, product-list.componente.html funcione.
 
 
 @Component({ //Esta linha indica que esta classe é um componente.
@@ -10,6 +11,7 @@ import {Input} from '@angular/core';//Esta linha importa o Ipunt da biblioteca, 
 export class ProductAlertsComponent implements OnInit {
 
   @Input() product;//Não entendi bem esta linha. Acredito que está associando o Input a classe pai product
+  @Output() notify = new EventEmitter();//Assim como a linha acima, estamos puxando a propriedade Output da importação na linha 3, e construindo um novo objeto EventEmitter
   constructor() { }
 
   ngOnInit() {
